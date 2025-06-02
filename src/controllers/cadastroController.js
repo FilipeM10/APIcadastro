1e131bef98a428d92d9d0f67f6149713f65d298b
FilipeM10 <filipefilie3089@gmail.com>
2025-06-01 22:53:37 -0300

    regras de uso da API completas

--- src/controllers/cadastroController.js
+++ src/controllers/cadastroController.js
@@ -4,5 +4,29 @@ export const listar = async (req,res) => {
     try {
         const dados = await cadastro.find()
         res.status(200).json({dados})
-    } catch(error) { console.log(error) }
+    } catch(error) { return res.status(500).json('Erro ao listar', error) }
+}
+export const procuraPorId = async (req,res) => {
+    try {
+        const {id} = req.params
+        const dados = await cadastro.findById(id)
+        if(!dados) return res.status(404).json({ mensage: 'Id não encontrado' });
+        return res.status(200).json({dados})
+    } catch(error) { return res.status(500).json(error) }
+}
+export const postar = async (req,res) => {
+    try {
+        const dados = await cadastro.create(req.body)
+        if(!dados) return req.status(404).json({ mensage: 'Adicione os dados necessários' })
+        return res.status(200).json({ dados })
+    } catch(error) { return req.status(500).json('Erro ao logar', error) }
+}
+export const deletar = async (req,res) => {
+    try{
+        const {id} = req.params
+        const dados = await cadastro.findByIdAndDelete(id)
+        if(!dados) res.status(404).json({ mensage: 'Id não encontrado' });
+        return res.status(200).json({dados})
+    } catch(error) { return req.status(500).json('Erro ao desinstalar', error) }
+
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,5 +4,8 @@ import * as controllers from '../controllers/cadastroController.js'
 const router = express.Router()
 
 router.get('/', controllers.listar)
+router.get('/:id', controllers.procuraPorId)
+router.post('/', controllers.postar)
+router.delete('/:id', controllers.deletar)
 
 export default router
